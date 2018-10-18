7ea5ee82c5d8462ddb8fd6111538dd533555f259
andersonribeir <38381171+andersonribeir@users.noreply.github.com>
2018-10-17 21:01:36 -0300

    Update q2.php

--- q2.php
+++ q2.php
@@ -8,7 +8,7 @@ $_SESSION['q1'] = $session1;
 <form method="GET" action="q3.php">
 		2+2<sup>2?=</sup> <br>
  		<input type="radio" name="valor" value="16"> 16<br>
- 		<input type="radio" name="valor" value="7"> 2<br>
+ 		<input type="radio" name="valor" value="2"> 7<br>
  		<input type="radio" name="valor" value="6"> 6<br><br>
 		<input type="submit" name="enviar" value="Enviar">
 
